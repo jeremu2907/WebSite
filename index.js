f0cdb1fa1591e47e6b67d79126ad7f709cf17ec7
jeremu2907 <jeremu2907@gmail.com>
2022-03-01 20:46:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function wait(){
     
     //Creating new terminal line
     setTimeout(() => {
-        document.getElementById("animate").innerHTML = "<div class='typing'><m style='color: #96FF74'>me@MyWebSite</m>:<m style='color: #52B7E1 '>/JEREMY_NGUYEN</m>$ More about me and my projects on my<a href='https://www.linkedin.com/in/jeremu2907/' class='link'> LinkedIn</a> and <a href='https://github.com/jeremu2907' class='link2'>GitHub</a></div>"
+        document.getElementById("animate").innerHTML = "<div class='typing intro'><m style='color: #96FF74'>me@MyWebSite</m>:<m style='color: #52B7E1 '>/JEREMY_NGUYEN</m>$ More about me and my projects on my<a href='https://www.linkedin.com/in/jeremu2907/' class='link'> LinkedIn</a> and <a href='https://github.com/jeremu2907' class='link2'>GitHub</a></div>"
     },4000);
 
     setTimeout(() => {
-        document.getElementById("animate").innerHTML = "<div class='typing2'><m style='color: #96FF74'>me@MyWebSite</m>:<m style='color: #52B7E1 ''>/JEREMY_NGUYEN</m>$ Email me at jeremu2907@gmail.com<div>"
+        document.getElementById("animate").innerHTML = "<div class='typing2 intro'><m style='color: #96FF74'>me@MyWebSite</m>:<m style='color: #52B7E1 ''>/JEREMY_NGUYEN</m>$ Email me at jeremu2907@gmail.com<div>"
     },10000)
 }
 wait();
